81dced625a60ddf56e0297e944d071ebc640c439
Yogesh Choudhary <yogesh986.be22@chitkara.edu.in>
2024-11-27 01:29:14 +0530

    adding multer to save profile pic

--- routes/register.js
+++ routes/register.js
@@ -1,26 +1,40 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 const otpStorage = require('../storage');
+const transporter = require("../transporter");
 
 require('dotenv').config();
 
-const transporter = require("../transporter")
+// Set up storage for Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/uploads'); // specify the 'uploads' folder
+    },
+    filename: (req, file, cb) => {
+        const username = req.body.username;
+        const ext = path.extname(file.originalname); // retain the file extension
+        cb(null, `${username}${ext}`); // Save file with username as the filename
+    }
+});
 
+// Initialize multer with the storage configuration
+const upload = multer({ storage: storage });
 
-
-router.post('/', (req, res) => {
+router.post('/', upload.single('image'), (req, res) => {
     const { name, username, skill, email, github, linkedin, password } = req.body;
 
-    
+    // Save registration data in session
     req.session.registrationData = { name, username, skill, email, github, linkedin, password };
 
+    // OTP generation logic
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
 
-   
     otpStorage[email] = otp;
-    console.log(otp+" "+otpStorage[email]);
+    console.log(otp + " " + otpStorage[email]);
 
-    
+    // Send OTP email
     const mailOptions = {
         from: 'your-email@example.com',
         to: email,
@@ -38,8 +52,6 @@ router.post('/', (req, res) => {
             res.redirect(`/otpVerification?email=${email}`);
         }
     });
-   
-    
 });
 
-module.exports = router
+module.exports = router;
